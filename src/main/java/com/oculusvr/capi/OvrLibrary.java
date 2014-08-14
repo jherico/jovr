b971ebef626a3992f6c5ec8c13e87ba1fd09a885
Brad Davis <bdavis@saintandreas.org>
2014-08-13 23:32:06 -0700

    Fixing OSX binaries

--- src/main/java/com/oculusvr/capi/OvrLibrary.java
+++ src/main/java/com/oculusvr/capi/OvrLibrary.java
@@ -24,7 +24,9 @@ import com.sun.jna.PointerType;
 public interface OvrLibrary extends Library {
   
   public static final String JNA_LIBRARY_NAME = "OVR_C";
+  // Used for testing debug builds
   // public static final String JNA_LIBRARY_NAME = "/Users/bdavis/Git/OculusRiftExamples/build64/output/OVR_Cd.dll";
+  // public static final String JNA_LIBRARY_NAME = "/Users/bdavis/git/OculusRiftExamples/build/output/libOVR_Cd.dylib";
   public static final NativeLibrary JNA_NATIVE_LIB = NativeLibrary.getInstance(OvrLibrary.JNA_LIBRARY_NAME);
   public static final OvrLibrary INSTANCE = (OvrLibrary) Native.loadLibrary(OvrLibrary.JNA_LIBRARY_NAME,
       OvrLibrary.class);
