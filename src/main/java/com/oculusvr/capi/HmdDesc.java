d7595b69b6b258651af74e0f668b827383759c97
crramirez <crramirez@users.noreply.github.com>
2015-12-29 10:58:57 -0500

    Update HmdDesc.java

--- src/main/java/com/oculusvr/capi/HmdDesc.java
+++ src/main/java/com/oculusvr/capi/HmdDesc.java
@@ -7,44 +7,44 @@ import java.util.List;
 
 public abstract class HmdDesc extends Structure {
 
-    public Pointer Handle;
-    /**
-     * @see ovrHmdType
-     */
-    public int Type;
-    public byte[] padding1 = new byte[4];
-    public Pointer ProductName;
-    public Pointer Manufacturer;
-    public short VendorId;
-    public short ProductId;
-    public byte[] SerialNumber = new byte[24];
-    public short FirmwareMajor;
-    public short FirmwareMinor;
-    public float CameraFrustumHFovInRadians;
-    public float CameraFrustumVFovInRadians;
-    public float CameraFrustumNearZInMeters;
-    public float CameraFrustumFarZInMeters;
-    public int HmdCaps;
-    public int TrackingCaps;
-    public FovPort[] DefaultEyeFov = new FovPort[2];
-    public FovPort[] MaxEyeFov = new FovPort[2];
-    public int[] EyeRenderOrder = new int[2];
-    public OvrSizei Resolution;
+  public Pointer Handle;
+  /**
+   * @see ovrHmdType
+   */
+  public int Type;
+  public byte[] padding1 = new byte[4];
+  public Pointer ProductName;
+  public Pointer Manufacturer;
+  public short VendorId;
+  public short ProductId;
+  public byte[] SerialNumber = new byte[24];
+  public short FirmwareMajor;
+  public short FirmwareMinor;
+  public float CameraFrustumHFovInRadians;
+  public float CameraFrustumVFovInRadians;
+  public float CameraFrustumNearZInMeters;
+  public float CameraFrustumFarZInMeters;
+  public int HmdCaps;
+  public int TrackingCaps;
+  public FovPort[] DefaultEyeFov = new FovPort[2];
+  public FovPort[] MaxEyeFov = new FovPort[2];
+  public int[] EyeRenderOrder = new int[2];
+  public OvrSizei Resolution;
 
-    @Override
-    protected List<?> getFieldOrder() {
-        return Arrays.asList("Handle", "Type", "padding1", "ProductName", "Manufacturer", "VendorId", "ProductId", "SerialNumber",
+  @Override
+  protected List<?> getFieldOrder() {
+    return Arrays.asList("Handle", "Type", "padding1", "ProductName", "Manufacturer", "VendorId", "ProductId", "SerialNumber",
                 "FirmwareMajor", "FirmwareMinor", "CameraFrustumHFovInRadians", "CameraFrustumVFovInRadians",
                 "CameraFrustumNearZInMeters", "CameraFrustumFarZInMeters", "HmdCaps", "TrackingCaps", "DefaultEyeFov",
                 "MaxEyeFov", "EyeRenderOrder", "Resolution");
-    }
+  }
 
-    public HmdDesc() {
-    }
+  public HmdDesc() {
+  }
 
-    public HmdDesc(Pointer p) {
-        super(p);
-        read();
-    }
+  public HmdDesc(Pointer peer) {
+    super(peer);
+    read();
+  }
 
 }
